3d12da7fea1293ffeb12ee3510af6d1bd6284587
Itisafeature <thebigdebugger@gmail.com>
2022-07-12 19:44:08 -0400

    work done on hamburger menu

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import logo from './logo.png';
 
 const HeaderContainer = styled.header`
   padding: 0.5rem 0.5rem 0 0.5rem;
   background: #264563;
+  display: flex;
 `;
 
 const LogoDiv = styled.div``;
@@ -18,7 +20,34 @@ const LogoImage = styled.img`
   vertical-align: middle;
 `;
 
+const HamburgerBar = styled.div`
+  width: 2.5rem;
+  height: 0.25rem;
+  background: black;
+  border-radius: 10px;
+  transition: all 0.3s linear;
+  transform-origin: 1px;
+`;
+
+const BarOne = styled(HamburgerBar)`
+  transform: ${props =>
+    props.mobileMenuShown ? 'rotate(45deg)' : 'rotate(0)'};
+`;
+
+const BarTwo = styled(HamburgerBar)`
+  opacity: ${props => (props.mobileMenuShown ? 0 : 1)};
+  transform: ${props =>
+    props.mobileMenuShown ? 'translateX(20px)' : 'translateX(0)'};
+`;
+
+const BarThree = styled(HamburgerBar)`
+  transform: ${props =>
+    props.mobileMenuShown ? 'rotate(-45deg)' : 'rotate(0)'};
+`;
+
 const Header = () => {
+  const [mobileMenuShown, setMobileMenuShown] = useState(false);
+
   return (
     <HeaderContainer>
       <LogoDiv>
@@ -26,6 +55,13 @@ const Header = () => {
           <LogoImage src={logo} />
         </a>
       </LogoDiv>
+      <div>
+        <div>
+          <BarOne></BarOne>
+          <BarTwo></BarTwo>
+          <BarThree></BarThree>
+        </div>
+      </div>
     </HeaderContainer>
   );
 };
